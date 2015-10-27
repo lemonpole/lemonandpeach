7ab54a5a2251b4471b278822f6e9bf4df49100d7
lemonpole <srivera.x1@gmail.com>
2015-10-26 21:10:58 -0400

    adds countdown to date directive

--- client/js/directives.js
+++ client/js/directives.js
@@ -15,3 +15,33 @@ app.directive( 'navToggle', function() {
     }
   };
 });
+
+app.directive( 'timer', function() {
+  return {
+    restrict: 'E',
+    scope: false,
+    controller: [ '$scope', '$interval', '$element', '$attrs', function( $scope, $interval, $element, $attrs ) {
+      function tick() {
+        var days = 24 * 60 * 60,
+            hours = 60 * 60,
+            minutes = 60;
+        var tillDate = new Date( $attrs.until );
+        var timeLeft = Math.floor( ( tillDate - new Date() ) / 1000 );
+
+        $scope.days = Math.floor( timeLeft / days );
+        timeLeft -= $scope.days * days;
+
+        $scope.hours = Math.floor( timeLeft / hours );
+        timeLeft -= $scope.hours * hours;
+
+        $scope.minutes = Math.floor( timeLeft / minutes );
+        timeLeft -= $scope.minutes * minutes;
+
+        $scope.seconds = timeLeft;
+      }
+
+      $interval( tick, 1000 );
+      tick();
+    }]
+  };
+});
