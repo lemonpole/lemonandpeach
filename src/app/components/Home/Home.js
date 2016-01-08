59e82aadcf9562e7bfbed8d7645c82337f96bec2
Steve Rivera <srivera.x1@gmail.com>
2016-01-08 15:06:44 -0500

    adds splash component

--- src/app/components/Home/Home.js
+++ src/app/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavCmp from '../Common/Nav';
+import SplashCmp from './Splash';
 
 class HomeCmp extends React.Component {
   render() {
     return (
       <div>
         <NavCmp />
+        <SplashCmp />
       </div>
     );
   }
---
+++ src/app/components/Home/Splash.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SplashCmp extends React.Component {
+  render() {
+    return (
+        <div id="splash"></div>
+    );
+  }
+}
+
+export default SplashCmp;
