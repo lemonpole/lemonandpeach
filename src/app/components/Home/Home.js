0bebaa0eca75425e046e23212266db165dfd5218
Steve Rivera <srivera.x1@gmail.com>
2016-01-08 15:38:10 -0500

    minor css adjustments. adds marriage date and fancy divider

--- src/app/components/Home/Home.js
+++ src/app/components/Home/Home.js
@@ -5,9 +5,20 @@ import SplashCmp from './Splash';
 class HomeCmp extends React.Component {
   render() {
     return (
-      <div>
+      <div className="full-height">
         <NavCmp />
         <SplashCmp />
+
+        <div id="married-date" className="container">
+          <h1>We are getting married!</h1>
+          <h2>Wednesday, May 25th, 2016</h2>
+        </div>
+
+        <div id="fancy-divider">
+          <hr />
+          <i className="fa fa-heart fa-lg fa-fw"></i>
+          <hr />
+        </div>
       </div>
     );
   }
