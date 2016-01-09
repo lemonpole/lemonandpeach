12547830137db3514279b8771786b564278db83a
lemonpole <srivera.x1@gmail.com>
2016-01-08 20:56:57 -0500

    added mobile support for main nav

--- src/app/components/Common/Nav.js
+++ src/app/components/Common/Nav.js
@@ -2,13 +2,33 @@ import React from 'react';
 import { Link } from 'react-router';
 
 class NavCmp extends React.Component {
+  constructor( props ) {
+    super( props );
+    this.state = this._getInitialState();
+  }
+
+  _getInitialState() {
+    return {
+      toggled: false
+    }
+  }
+
+  _handleClick() {
+    this.setState({ toggled: !this.state.toggled });
+  }
+
   render() {
     return (
       <nav>
+        <div id="bars" onClick={ this._handleClick.bind( this ) }>
+          <i className="fa fa-bars fa-2x"></i>
+        </div>
+
         <div id="logo-container">
           <img src="http://placehold.it/350x60" alt="Lemon and Peach" />
         </div>
-        <ul id="items-container">
+
+        <ul id="items-container" className={ ( this.state.toggled ) ? 'toggled' : '' }>
           <li><a href="#">Home</a></li>
           <li><Link to="/rsvp">RSVP</Link></li>
           <li><a href="#">Info</a></li>
