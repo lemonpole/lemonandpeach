dabeaf801876841fc60582af73e9fc85a426cd2c
lemonpole <srivera.x1@gmail.com>
2016-01-23 20:34:13 -0500

    polished up rsvp page. added going/not going toggle functionality

--- src/app/components/RSVP/RSVP.js
+++ src/app/components/RSVP/RSVP.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router';
 import NavCmp from '../Common/Nav';
 
 class RSVPCmp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isGoing: false
+    };
+  }
+
+  onClickGoing( event ) {
+    event.preventDefault();
+    this.setState({ isGoing: 'going' });
+  }
+
+  onClickNotGoing( event ) {
+    event.preventDefault();
+    this.setState({ isGoing: 'not going' });
+  }
+
   render() {
     return (
         <div id="rsvp">
@@ -21,16 +39,32 @@ class RSVPCmp extends React.Component {
 
                 <div className="row">
                   <div className="col-md-6">
-                    <button className="btn btn-success btn-lg btn-block">
+                    <button className="btn btn-going btn-lg btn-block" onClick={ this.onClickGoing.bind( this ) }>
                       The fuck yea!
                     </button>
                   </div>
                   <div className="col-md-6">
-                    <button className="btn btn-danger btn-lg btn-block">
+                    <button className="btn btn-not-going btn-lg btn-block" onClick={ this.onClickNotGoing.bind( this ) }>
                       No, I am a dookiehead!
                     </button>
                   </div>
                 </div>
+
+                <div id="going" className={ 'form-group ' + ( this.state.isGoing == 'going' ? 'show' : 'hide' ) }>
+                  <h3>Yay! So, who's your plus one?</h3>
+                  <input type="text" className="form-control" placeholder="Their full name, please!" />
+                </div>
+
+                <div className={ 'form-group ' + ( this.state.isGoing == 'not going' ? 'show' : 'hide' ) }>
+                  <h3>
+                    Aww! Too bad. Atleast, you can always contribute to our <Link to="/fund">Honeymoon Fund</Link>.
+                  </h3>
+                  <img src="http://www.reactiongifs.com/wp-content/uploads/2012/06/when-you-try-to-wink1.gif" />
+                </div>
+
+                <button className={ 'btn btn-submit btn-lg btn-block ' + ( !this.state.isGoing ? 'hide' : 'show' ) }>
+                  Submit
+                </button>
               </form>
             </div>
           </div>
