30bbea740325a71ba5c788226bc8eb711c5f491f
lemonpole <srivera.x1@gmail.com>
2016-03-28 23:24:50 -0400

    adds map to info component

--- src/app/components/Info/Info.js
+++ src/app/components/Info/Info.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router';
 import NavCmp from '../Common/Nav';
+import { GoogleMap, Marker } from 'react-google-maps';
+import { default as ScriptjsLoader } from 'react-google-maps/lib/async/ScriptjsLoader';
 
 class InfoCmp extends React.Component {
   constructor( props ) {
@@ -11,6 +13,26 @@ class InfoCmp extends React.Component {
     return (
       <div id="info">
         <NavCmp location={ this.props.location } />
+
+        <ScriptjsLoader
+          hostname={ 'maps.googleapis.com' }
+          pathname={ '/maps/api/js' }
+          query={{ v: '3.22', libraries: 'geometry,drawing,places' }}
+          loadingElement={
+            <div><i className="fa fa-spinner fa-spin"></i></div>
+          }
+          containerElement={
+            <div className="google-map" />
+          }
+          googleMapElement={
+            <GoogleMap
+              defaultZoom={ 18 }
+              defaultCenter={{ lat: 39.9485890, lng: -75.1730360 }}
+            >
+              <Marker position={{ lat: 39.9485890, lng: -75.1730360 }} defaultAnimation={ 2 } />
+            </GoogleMap>
+          }
+        />
       </div>
     );
   }
