d9710bd95112cbb55076a6ede08727180f705d7d
Steve Rivera <srivera.x1@gmail.com>
2016-03-29 15:25:51 -0400

    google map loads marker and tooltip with location information

--- src/app/components/Info/Info.js
+++ src/app/components/Info/Info.js
@@ -1,20 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router';
 import NavCmp from '../Common/Nav';
-import { GoogleMap, Marker } from 'react-google-maps';
-import { default as ScriptjsLoader } from 'react-google-maps/lib/async/ScriptjsLoader';
+import { GoogleMapLoader, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+
+const ADDRESS = '1906 Rittenhouse Square, Philadelphia, PA 19103';
+const DEFAULT_ZOOM = 15;
+
+let geocoder = new google.maps.Geocoder();
 
 class InfoCmp extends React.Component {
   constructor( props ) {
     super( props );
+    this.state = {
+      result: false
+    };
   }
 
+  componentDidMount() {
+    geocoder.geocode({ address: ADDRESS }, ( results, status ) => {
+      if( status == google.maps.GeocoderStatus.OK ) {
+        this.setState({ result: results[ 0 ] });
+      } else {
+        console.log( status );
+      }
+    });
+  }
+
+  // TODO: refactor, create into reusable components
   render() {
+    if( !this.state.result ) {
+      return (
+        <div id="info">
+          <NavCmp location={ this.props.location } />
+          <div className="google-map">
+            <i className="fa fa-spinner fa-spin"></i>
+          </div>
+        </div>
+      );
+    }
+
     return (
       <div id="info">
         <NavCmp location={ this.props.location } />
 
-        <ScriptjsLoader
+        <GoogleMapLoader
           hostname={ 'maps.googleapis.com' }
           pathname={ '/maps/api/js' }
           query={{ v: '3.22', libraries: 'geometry,drawing,places' }}
@@ -26,10 +55,18 @@ class InfoCmp extends React.Component {
           }
           googleMapElement={
             <GoogleMap
-              defaultZoom={ 18 }
-              defaultCenter={{ lat: 39.9485890, lng: -75.1730360 }}
+              defaultZoom={ DEFAULT_ZOOM }
+              defaultCenter={ this.state.result.geometry.location }
             >
-              <Marker position={{ lat: 39.9485890, lng: -75.1730360 }} defaultAnimation={ 2 } />
+              <Marker position={ this.state.result.geometry.location } defaultAnimation={ 2 }>
+                <InfoWindow>
+                  <div>
+                    <b>Philadelphia Ethical Society</b><br />
+                    1906 Rittenhouse Square<br />
+                    Philadelphia, PA 19103
+                  </div>
+                </InfoWindow>
+              </Marker> 
             </GoogleMap>
           }
         />
